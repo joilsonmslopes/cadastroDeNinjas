c6f71115fa5b5083e68ad213a704bf7607e6cdd1
Joilson Miranda <joilsonmirandasl@hotmail.com>
2025-05-22 11:13:27 -0300

    feat: add missionRepository with japRepository

---
+++ src/main/java/com/joilson/CadastroDeNinjas/Missions/MissionsRepository.java
@@ -0,0 +1,7 @@
+package com.joilson.CadastroDeNinjas.Missions;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissionsRepository extends JpaRepository<MissionsModel, Long> {
+
+}
