da4cb4685258a90f97d18be3034999de7e35c373
Joilson Miranda <joilsonmirandasl@hotmail.com>
2025-05-22 22:23:25 -0300

    feat: create ninjaRepository

---
+++ src/main/java/com/joilson/CadastroDeNinjas/Ninjas/NinjasRepository.java
@@ -0,0 +1,5 @@
+package com.joilson.CadastroDeNinjas.Ninjas;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjasRepository extends JpaRepository<NinjaModel, Long> {}
