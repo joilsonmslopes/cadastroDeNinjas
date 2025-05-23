cd5665236e58c7870b1039423a90760373553df8
Joilson Miranda <joilsonmirandasl@hotmail.com>
2025-05-23 14:16:29 -0300

    feat: update ninjaController with mock data

--- src/main/java/com/joilson/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/com/joilson/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package com.joilson.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,5 +9,35 @@ public class NinjaController {
     @GetMapping("boas-vindas")
     public String boasVindas() {
         return "Hello World! Bem vindo ao SpringBoot!";
+    }
+
+    // adicionar ninja
+    @PostMapping("cadastrar")
+    public String createNinja() {
+        return "Ninja Adicionado com sucesso";
+    }
+
+    // listar todos os ninjas
+    @GetMapping("listar")
+    public String getNinjas() {
+        return "Lista de ninjas";
+    }
+
+    // buscar ninja por ID
+    @GetMapping("listarId")
+    public String getNinjaById() {
+        return "Meu Ninja";
+    }
+
+    // atualizar informações de um ninja
+    @PutMapping("atualizar")
+    public String updateNinja() {
+        return "Ninja atualizad com sucesso";
+    }
+
+    // deletar um ninja
+    @DeleteMapping("deletar")
+    public String deleteNinja() {
+        return "Ninja deletado com sucesso";
     }
 }
